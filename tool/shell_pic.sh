02fdfb45a7393a2af114b6c2a501e4fac7034779
zhuxu <zhuxu@baidu.com>
2017-07-30 16:14:17 +0800

    =update

--- tool/shell_pic.sh
+++ tool/shell_pic.sh
@@ -2,11 +2,11 @@ echo $1;
 rm -rf $1/img/output;
 mkdir $1/img/output;
 echo "output:"$1
-echo "resize photos"
+echo "resize photos1"
 cd $1/img
-find . -type f -name "*.jpg"|xargs -I {} convert -resize 1240x1240 {} output/{}
+find . -type f -name "*.jpg" -o -name "*.JPG"|xargs -I {} convert -resize 1240x1240 {} output/{}
 echo "compress photos"
 cd output
-find . -type f -name "*.jpg"|xargs -I {} jpegoptim  -m80 {}
+find . -type f -name "*.jpg" -o -name "*.JPG"|xargs -I {} jpegoptim  -m80 {}
 
 
