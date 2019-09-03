30006d696605d5f965695812ed080241fcbc4f91
zhuxu <zhuxu@baidu.com>
2019-09-03 14:55:24 +0800

    =去掉上传简书

--- tool/init.py
+++ tool/init.py
@@ -16,5 +16,5 @@ shell_path = '/Users/zhuxu/Documents/mmjstool/write/tool/shell_pic.sh'
 os.system(shell_path + ' .')
 
 
-js = Jianshu()
-js.initArt(art_dir_path)
+# js = Jianshu()
+# js.initArt(art_dir_path)
